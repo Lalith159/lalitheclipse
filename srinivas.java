1bae466dda56231ad7d15b3b355ded44df4e9f89
Lalith159 <155241435+Lalith159@users.noreply.github.com>
2024-01-29 23:40:05 +0530

    Add files via upload

---
+++ srinivas.java
@@ -0,0 +1,18 @@
+package parentsapplication;
+
+public class srinivas {
+	
+	    public String name ;
+	    private int age;
+	    public void myaddress(String address) {
+	    	System.out.println("the address:"+address);
+	    	
+	    }
+	    public void parentname(String name) {	  
+	  	  System.out.println("the name of the parent:="+name);
+	    }
+	    public void age(int age) {	  
+	  	  System.out.println("the age of the parent:="+age);
+	    }
+	}
+
